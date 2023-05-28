df4e32f8dc3acfe66d8e9364249bc7b5c31adfde
go-timothy <timothy.dang.goldenowl@gmail.com>
2023-05-28 09:50:34 +0700

    add task and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_142816) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_28_023338) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,5 +29,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_142816) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.text "title"
+    t.text "detail"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "username"
+    t.text "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "tasks", "users"
 end
