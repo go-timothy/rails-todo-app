9dce62615b1205cfe975568e04bfb2935511ebf7
go-timothy <timothy.dang.goldenowl@gmail.com>
2023-05-28 21:01:37 +0700

    setup devise for todo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  devise_for :users
   root "tasks#index"
-
-  resources :articles do
-    resources :comments
-  end
-
   resources :tasks
 end
