7d2fe89119ba4c453f89931f23df04b30e5643b9
go-timothy <timothy.dang.goldenowl@gmail.com>
2023-05-29 09:55:47 +0700

    fix tasks show all not correct user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :detail, presence: true, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "tasks#index"
   resources :tasks
+
+  resources :articles do
+    resources :comments
+  end
 end
